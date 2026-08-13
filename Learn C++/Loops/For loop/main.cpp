0b80b15bb9b0ee6cc834da62e18f293bdadba54d
Samrdhi-SinGh <singhsamridhi878@gmail.com>
2026-08-13 21:47:07 +0530

    Loops

---
+++ Learn C++/Loops/For loop/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+  for (int i = 0; i <= 10; i++)
+  {
+    // continue;//No output
+    cout << i << endl;
+    // break; //0
+  }
+  return 0;
+}
